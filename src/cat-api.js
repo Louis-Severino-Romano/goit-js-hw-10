b85217c1fabbdf8a0b4f54ab6f4edb55eec30002
Louis-Severino-Romano <louis.romano@ctu.edu.ph>
2024-04-23 16:21:13 +0800

    in

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,6 +1,8 @@
-const baseUrl=
-const api_key=live_tKr4IYSQLzyDcnpfmmXMohRhtJFemy4XW1xBvdKb3wBzX1f7kQfxOKmng3gzh4C8;
+// const baseUrl=https://api.thecatapi.com/v1/images/search;
+// const api_key=live_tKr4IYSQLzyDcnpfmmXMohRhtJFemy4XW1xBvdKb3wBzX1f7kQfxOKmng3gzh4C8;
 
-function fetchBreeds(){
-fetch('')
-}
+// export function fetchBreeds(){
+//     return fetch('https://api.thecatapi.com/v1/images/search?x-api-key=live_tKr4IYSQLzyDcnpfmmXMohRhtJFemy4XW1xBvdKb3wBzX1f7kQfxOKmng3gzh4C8')
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+// }
---
+++ src/index.js
@@ -0,0 +1,7 @@
+function fetchBreeds(){
+    return fetch('https://api.thecatapi.com/v1/breeds?x-api-key=live_tKr4IYSQLzyDcnpfmmXMohRhtJFemy4XW1xBvdKb3wBzX1f7kQfxOKmng3gzh4C8')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+
+fetchBreeds();
