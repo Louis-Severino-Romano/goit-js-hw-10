52b71d8b2821d293b92b42b70708843ac2044e10
Louis-Severino-Romano <143082355+Louis-Severino-Romano@users.noreply.github.com>
2024-04-23 08:21:41 +0000

    Deploying to gh-pages from @ Louis-Severino-Romano/goit-js-hw-10@b85217c1fabbdf8a0b4f54ab6f4edb55eec30002 🚀

---
+++ index.0988f304.js
@@ -0,0 +1,2 @@
+fetch("https://api.thecatapi.com/v1/breeds?x-api-key=live_tKr4IYSQLzyDcnpfmmXMohRhtJFemy4XW1xBvdKb3wBzX1f7kQfxOKmng3gzh4C8").then((function(t){return t.json()})).then((function(t){return console.log(t)}));
+//# sourceMappingURL=index.0988f304.js.map
